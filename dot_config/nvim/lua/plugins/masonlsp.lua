12f0025343c9c63cf8aaed03f6542b91384f2290
Pradeep <219465716+pradeepcodes1@users.noreply.github.com>
2025-12-19 16:36:32 +0530

    refactor(nvim): update LSP configuration and suppress deprecation warnings
    - Refactor LSP setup to use 'vim.lsp.config' and 'vim.lsp.enable'
    - Suppress specific deprecation warnings in init.lua
    - Add homebrew Cellar to ignored paths
    - Remove redundant diffview configuration

--- dot_config/nvim/lua/plugins/masonlsp.lua
+++ dot_config/nvim/lua/plugins/masonlsp.lua
@@ -13,13 +13,14 @@ return {
 					function(server_name) -- default handler (optional)
 						print("TESTING: " .. server_name)
 						vim.notify("Setting up LSP: " .. server_name, vim.log.levels.INFO)
-						require("lspconfig")[server_name].setup({
+						vim.lsp.config(server_name, {
 							on_attach = common.on_attach,
 							capabilities = capabilities,
 							handlers = {
 								["$/progress"] = function() end, -- disable progress notifications
 							},
 						})
+						vim.lsp.enable(server_name)
 					end,
 					jdtls = function() end,
 				},
